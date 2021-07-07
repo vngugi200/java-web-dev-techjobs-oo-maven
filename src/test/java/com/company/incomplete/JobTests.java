45c8e69335a3b4619bee4a08b2898693f2fb1b5d
pdmxdd <pdmxdd@gmail.com>
2021-07-07 00:02:47 -0500

    starter code

--- src/test/java/com/company/incomplete/JobTests.java
+++ src/test/java/com/company/incomplete/JobTests.java
@@ -1,4 +1,38 @@
 package com.company.incomplete;
 
+import org.junit.jupiter.api.Test;
+
 public class JobTests {
+    // you can rename the tests, or delete them and rewrite them from scratch, but I went ahead and provided you the names of tests I created -- I followed the writeup pretty honestly
+    @Test
+    public void testSettingJobId() {
+
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+
+    }
+
+    @Test
+    public void testJobsForEquality() {
+
+    }
+
+    // I found the instructions around the three toString() tests a little confusing, so I created three tests following the three bullet points:
+    // you can follow TDD if you want, or you can write the implementation first and then get the tests to pass, either is a fine approach
+    @Test
+    public void testToStringContainsBlankLines() {
+
+    }
+
+    @Test
+    public void testToStringHasLabelsForEachField() {
+
+    }
+
+    @Test
+    public void testToStringDataNotAvailable() {
+
+    }
 }
