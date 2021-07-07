3cc734c0b0c173cc0dfe6d485f5b48f78ad45c3a
pdmxdd <pdmxdd@gmail.com>
2021-07-06 23:57:05 -0500

    updated sanity check in main

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,10 +1,12 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 //    public static void main(String[] args) {
 //        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
 //        Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
-//        Job job3 = new Job("Ice cream taster", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Taste"));
+//        Job job3 = new Job("Ice cream taster", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Tasting ability"));
 //
 //        ArrayList<Job> jobs = new ArrayList<>();
 //        jobs.add(job1);
